4e51f0ecdacd6ab90a591acf95acb9edfa794dae
Heather Kirk <hskirk@gmail.com>
2019-03-25 00:55:46 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -5,4 +5,4 @@ INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergea
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 3, "female", "stoic", 1)
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 7, "male", "aggressive", 0)
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 8, "female", "docile", 0)
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES 
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 12, "male", "aggressive", 1)
