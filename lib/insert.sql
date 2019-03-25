60de4f9b9d4f1c69de3e6978627871709753425b
Heather Kirk <hskirk@gmail.com>
2019-03-25 00:51:03 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,2 +1,2 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 3, "male", "brown", "docile", 1)
-INSERT INTO bears
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
